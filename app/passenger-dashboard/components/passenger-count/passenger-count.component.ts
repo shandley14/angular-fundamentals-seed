705f446705d14b7d8067f73b694103ffdaea70b0
Shandley Phillips <shandley.phillips@sharp.com>
2017-10-25 15:27:56 -0700

    Event Emitting Finished

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input} from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+    selector: 'passenger-count',
+    template: `
+    <h3>Airline Passengers</h3>
+    <div>
+       Total checked in: {{checkedInCount()}}/{{items.length}} 
+    </div>
+    `
+})
+
+export class PassengerCountComponent {
+    @Input()
+    items: Passenger[];
+    checkedInCount(): number{
+        if(!this.items) return;
+
+        return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+    }
+}
---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+
+@NgModule({
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PassengerDashboardComponent
+  ]
+})
+export class PassengerDashboardModule {}
